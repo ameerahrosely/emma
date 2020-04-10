618bbc29d408a86e6f6e3d0660c880f9e9a7bed5
Azri <azri4@Azris-iMac.local>
2020-04-10 23:24:50 +0800

    Adding Input Fields

--- lib/main.dart
+++ lib/main.dart
@@ -7,109 +7,109 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      
       home: Scaffold(
-        backgroundColor: Colors.black87,
+        backgroundColor: Colors.pink[50],
+        
         body: SafeArea(
           child: Column(
+
+            //mainAxisAlignment: MainAxisAlignment.center,
+
             children: <Widget>[
-              Image.asset('images/emma3.png', width: 150.0, height: 150.0,),
+              
+              Image.asset('images/emma3.png', width: 200.0, height: 200.0,),
+              
+              SizedBox(
+                height: 30.0,
+              ),
+
               Container(
-                child: Row(
-                  children: <Widget>[
-                    Text('Sign Up')
-                  ],
+             
+                child: Text(
+                  'Sign Up',
+                  style: TextStyle(
+                    fontSize: 40.0,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+
+              SizedBox(
+                height: 30.0,
+              ),
+
+              Card(
+                margin: EdgeInsets.symmetric(horizontal: 30.0, vertical: 10.0),
+                color: Colors.white,
+                child: ListTile(
+                  leading: Icon(
+                    Icons.person,
+                    color: Colors.black,
+                  ),
+                  title: TextFormField(
+                    decoration: InputDecoration(
+                      hintText: 'Enter your name',
+                    ),
+                  ),
                 )
-              )
+              ),
+
+              Card(
+                margin: EdgeInsets.symmetric(horizontal: 30.0, vertical: 10.0),
+                color: Colors.white,
+                child: ListTile(
+                  leading: Icon(
+                    Icons.email,
+                    color: Colors.black,
+                  ),
+                  title: TextFormField(
+                    decoration: InputDecoration(
+                      hintText: 'Enter your email',
+                    ),
+                  ),
+                )
+              ),
+
+              Card(
+                margin: EdgeInsets.symmetric(horizontal: 30.0, vertical: 10.0),
+                color: Colors.white,
+                child: ListTile(
+                  leading: Icon(
+                    Icons.lock,
+                    color: Colors.black,
+                  ),
+                  title: TextFormField(
+                    decoration: InputDecoration(
+                      hintText: 'Enter your password',
+                    ),
+                  ),
+                )
+              ),
+
+              Card(
+                margin: EdgeInsets.symmetric(horizontal: 30.0, vertical: 10.0),
+                color: Colors.white,
+                child: ListTile(
+                  leading: Icon(
+                    Icons.lock,
+                    color: Colors.black54,
+                  ),
+                  title: TextFormField(
+                    decoration: InputDecoration(
+                      hintText: 'Re-enter your password',
+                    ),
+                  ),
+                )
+              ),
+
             ],
           ),
         ),
+      
       ),
       
     );
   }
 }
-
-/*class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }
-}*/
